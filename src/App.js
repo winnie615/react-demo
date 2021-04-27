9ca99a6a893fc17b60029c4f1a744fd37246bead
winnie615 <31118941+winnie615@users.noreply.github.com>
2021-04-27 16:19:07 +0800

    router

--- src/App.js
+++ src/App.js
@@ -2,35 +2,18 @@
  * @Author: Winnie
  * @Date: 2021-04-10 09:58:06
  * @LastEditors: Winnie
- * @LastEditTime: 2021-04-19 13:57:24
+ * @LastEditTime: 2021-04-27 16:03:39
  * @Description: 作用
  * @FilePath: \react-demo\src\App.js
  */
 // import './App.css';
 import 'antd/dist/antd.css'
-// import ContextPage from './pages/lesson1/ContextPage';
-// import HocPage from './pages/lesson1/HocPage';
-// import MyRCForm from './pages/lesson2/MyRcForm';
-// import FormPage  from './pages/lesson2/FormPage';
-// import MyRCFieldForm  from './pages/lesson4/MyRCFieldForm';
-// import DialogPage  from './pages/react/lesson5/DialogPage';
-// import ReducerPage  from './pages/redux/ReducerPage';
-// import ReduxPage  from './pages/redux/ReduxPage';
-// import ReactReduxPage  from './pages/redux/ReactReduxPage';
-import HooksPage  from './pages/react-redux/HooksPage';
+import RouterPage  from './pages/react-router/RouterPage';
 
 function App() {
   return (
     <div className="App">
-      {/* <ContextPage/> */}
-      {/* <HocPage/> */}
-      {/* <MyRCForm/> */}
-      {/* <FormPage/> */}
-      {/* <MyRCFieldForm/> */}
-      {/* <DialogPage/> */}
-      {/* <ReduxPage/> */}
-      {/* <ReactReduxPage/> */}
-      <HooksPage/>
+      <RouterPage/>
     </div>
   );
 }
